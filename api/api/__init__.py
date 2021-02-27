cf0ae46b05de6885c513f0b905c78d8473cb1cc5
Jan-Willem van der Sijp <gecko-git@parseq.co.uk>
2021-02-27 17:09:07 +0100

    Allow setting of loglevel in environment

--- api/api/__init__.py
+++ api/api/__init__.py
@@ -1,17 +1,19 @@
 import logging
+import os
 import sys
 
 __version__ = '0.1.0'
 
 
 def setup_logging() -> None:
+    level = os.getenv("LOGLEVEL", "INFO")
     formatter = logging.Formatter(fmt="[%(asctime)s %(levelname)s]: %(message)s")
     handler = logging.StreamHandler(sys.stdout)
     handler.setFormatter(formatter)
 
     rootlogger = logging.getLogger(__name__)
     rootlogger.addHandler(handler)
-    rootlogger.setLevel(logging.DEBUG)
+    rootlogger.setLevel(level)
     rootlogger.propagate = False
 
 
